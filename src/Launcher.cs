4bc5b103eb3ddd1c8b4296170881a3540916edb5
Adam Gardiner <adam.b.gardiner@gmail.com>
2012-09-04 17:00:19 +0100

    Improve Launcher functionality

--- src/Launcher.cs
+++ src/Launcher.cs
@@ -46,10 +46,10 @@ namespace HFMCmd
     /// </summary>
     public class Launcher
     {
-
         // Reference to class logger
         protected static readonly ILog _log = LogManager.GetLogger(
             System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
 
         /// <summary>
         /// Static public flag indicating that the application is to terminate
@@ -70,34 +70,8 @@ namespace HFMCmd
             Win32.Handler hr = new Win32.Handler(CtrlHandler);
             Win32.SetConsoleCtrlHandler(hr, true);
 
-            ConfigureLogging();
-
-            // Register commands
-            Registry commands = Registry.FindCommands("HFM");
-
-            // TODO: Process command-line arguments
-            var cmdLine = new UI(HFMCmd.Resource.Help.Purpose);
-            var arg = cmdLine.AddPositionalArgument("CommandOrFile",
-                    "The name of the command to execute, or the path to a file containing commands to execute");
-            arg.Validate = (string argVal, out string errorMsg) => {
-                errorMsg = String.Format("File not found: {0}", argVal);
-                return commands.Contains(argVal) || File.Exists(argVal);
-            };
-
-
-            arg.OnParse = (key, val) => {
-            };
-
-            cmdLine.AddKeywordArgument("Domain", "The domain to which the user should be validated in");
-            cmdLine.AddKeywordArgument("UserName", "The user id to use to connect to HFM");
-            cmdLine.AddKeywordArgument("Password", "The password to use to connect to HFM");
-            cmdLine.AddKeywordArgument("Host", "The HFM cluster or server to connect to");
-            cmdLine.AddFlagArgument("Debug", "Enable debug logging");
-            var args = cmdLine.Parse(Environment.GetCommandLineArgs());
-
-            var Context = new Context(commands);
-            Context.Set(new HFM.Client());
-            Context.Invoke("SetLogonInfo", args);
+            // Run the application
+            new Application().Run();
 
             // This line needs to appear at the end of the prgram code as a marker to
             // the GC so that it does not collect our control-key handler
@@ -120,11 +94,53 @@ namespace HFMCmd
             return true;
         }
 
+    }
+
+
+    public class Application
+    {
+        // Reference to class logger
+        protected static readonly ILog _log = LogManager.GetLogger(
+            System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private Registry _commands;
+        private Context _context;
+        private UI _cmdLine;
+
+
+        /// <summary>
+        /// Main program entry point
+        /// </summary>
+        public void Run()
+        {
+            ConfigureLogging();
+
+            // Register commands
+            _commands = Registry.FindCommands("HFMCmd");
+            _commands = Registry.FindCommands("HFM", _commands);
+
+            // TODO: Process command-line arguments
+            _cmdLine = new UI(HFMCmd.Resource.Help.Purpose);
+            var arg = _cmdLine.AddPositionalArgument("CommandOrFile",
+                    "The name of the command to execute, or the path to a file containing commands to execute");
+            arg.Validate = ValidateCommand;
+
+
+            // Standard command-line arguments
+            _cmdLine.AddFlagArgument("Debug", "Enable debug logging");
+            var args = _cmdLine.Parse(Environment.GetCommandLineArgs());
+
+            _context = new Context(_commands);
+            _context.Set(this);
+            //_context.Set(new HFM.Client());
+            _context.Invoke(args["CommandOrFile"] as string, args);
+        }
+
 
         /// <summary>
         /// Configures logging.
         /// </summary>
-        protected static void ConfigureLogging()
+        protected void ConfigureLogging()
         {
             // Create a console logger
             ConsoleAppender ca = new ConsoleAppender();
@@ -141,5 +157,50 @@ namespace HFMCmd
             logHier.Root.Level = log4net.Core.Level.Debug;
         }
 
+
+        protected bool ValidateCommand(string argVal, out string errorMsg)
+        {
+            bool ok;
+            errorMsg = String.Format("Command file '{0}' not found", argVal);
+            if(_commands.Contains(argVal)) {
+                ok = true;
+
+                // TODO: Add command arguments as keyword args
+                _log.DebugFormat("Adding keyword args for {0} command", argVal);
+                Command.Command cmd = _commands[argVal];
+
+                if(cmd.Namespace == "HFM") {
+                    // Add standard arguments for logging in
+                    _cmdLine.AddKeywordArgument("Domain", "The domain to which the user should be validated in");
+                    _cmdLine.AddKeywordArgument("UserName", "The user id to use to connect to HFM");
+                    _cmdLine.AddKeywordArgument("Password", "The password to use to connect to HFM");
+                    _cmdLine.AddKeywordArgument("Host", "The HFM cluster or server to connect to");
+                }
+
+                // Add additional arguments needed by the command
+                foreach(var param in cmd.Parameters) {
+                    _log.DebugFormat("Adding keyword arg {0}", param.Name);
+                    _cmdLine.AddKeywordArgument(param.Name, "");
+                }
+            }
+            else {
+                ok = File.Exists(argVal);
+            }
+            return ok;
+        }
+
+
+
+        [Command]
+        public void Log([DefaultValue("INFO")] string level,
+                        [DefaultValue(null)]   string logFile)
+        {
+            log4net.Repository.ILoggerRepository repo = LogManager.GetRepository();
+            Hierarchy logHier = (Hierarchy)repo;
+            logHier.Root.Level = repo.LevelMap[level];
+        }
+
+
     }
+
 }
