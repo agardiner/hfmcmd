274c04fc1d80f3415d4cfd7c192468136a4c1d95
Adam Gardiner <adam.gardiner@vodafone.com>
2012-09-07 17:37:24 +0100

    Add Log4NetExtensions.cs
    Adds support for Fine and Trace log levels, as well as support for
    rendering exceptions a little better.

---
+++ src/Log4NetExtensions.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Runtime.InteropServices;
+
+using log4net;
+using log4net.ObjectRenderer;
+using log4net.Repository.Hierarchy;
+
+
+
+namespace HFMCmd
+{
+
+    /// <summary>
+    /// Extension methods for adding additional log shortcuts for Verbose and
+    /// Trace level logging.
+    /// </summary>
+    public static class ILogExtentions
+    {
+
+        public static void Fine(this ILog log, string message, Exception exception)
+        {
+            log.Logger.Log(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType,
+                log4net.Core.Level.Fine, message, exception);
+        }
+
+        public static void Fine(this ILog log, string message)
+        {
+            log.Fine(message, null);
+        }
+
+        public static void FineFormat(this ILog log, string message, params object[] args)
+        {
+            if(log.Logger.IsEnabledFor(log4net.Core.Level.Fine)) {
+                log.Fine(string.Format(message, args), null);
+            }
+        }
+
+        public static void Trace(this ILog log, string message, Exception exception)
+        {
+            log.Logger.Log(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType,
+                log4net.Core.Level.Trace, message, exception);
+        }
+
+        public static void Trace(this ILog log, string message)
+        {
+            log.Trace(message, null);
+        }
+
+        public static void TraceFormat(this ILog log, string message, params object[] args)
+        {
+            if(log.Logger.IsEnabledFor(log4net.Core.Level.Trace)) {
+                log.Trace(string.Format(message, args), null);
+            }
+        }
+
+    }
+
+
+    /// <summary>
+    /// Class for handling logging of exceptions. Used to avoid the default
+    /// behaviour of logging a full back-trace when an exception occurs.
+    /// </summary>
+    public sealed class ExceptionMessageRenderer : IObjectRenderer
+    {
+
+        /// Reference to the logger hierarchy
+        private Hierarchy _logHierarchy;
+
+        /// Constructor
+        public ExceptionMessageRenderer()
+        {
+            _logHierarchy = LogManager.GetRepository() as Hierarchy;
+        }
+
+
+        /// Implementation of the IObjectRenderer interface. Called on when an
+        /// exception is logged.
+        public void RenderObject(RendererMap map, object obj, System.IO.TextWriter writer)
+        {
+            Exception ex = obj as Exception;
+            string stackTrace = ex.StackTrace;
+
+            for (; ex != null; ex = ex.InnerException) {
+                if (ex is COMException && ex.Message.StartsWith("<?xml")) {
+                    writer.Write("HFMException message XML contents:");
+                    writer.Write(ex);
+                }
+                else {
+                    writer.Write(ex.GetType().Name);
+                    writer.Write(": ");
+                    writer.Write(ex.Message);
+                }
+                if (ex.InnerException != null) {
+                    writer.WriteLine();
+                }
+            }
+
+            if (_logHierarchy.Root.Level.CompareTo(log4net.Core.Level.Fine) < 0) {
+                writer.WriteLine();
+                writer.WriteLine();
+                writer.WriteLine("Backtrace:");
+                writer.Write(stackTrace);
+            }
+        }
+    }
+
+}
