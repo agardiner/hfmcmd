1d9290b589fda6ec5f614a2ef7d14a733fcb8bc9
Adam Gardiner <adam.b.gardiner@gmail.com>
2012-09-20 13:36:46 +0100

    Reorganise Parameter and Setting attributes into class hierarchy

--- src/command/Attributes.cs
+++ src/command/Attributes.cs
@@ -24,6 +24,7 @@ namespace Command
     }
 
 
+
     /// </summary>
     /// Define an attribute that can be used to tag a method, property, or
     /// constructor as a source of new instances of the class returned by
@@ -36,6 +37,7 @@ namespace Command
     public class FactoryAttribute : Attribute
     {
     }
+
 
 
     /// </summary>
@@ -52,52 +54,40 @@ namespace Command
     }
 
 
+
     /// <summary>
-    /// Used to define the possible settings that can appear in an
-    /// ISettingsCollection subclass. These often change between versions, so
-    /// Since and Deprecated arguments are supported.
+    /// Abstract base class for Parameter and Setting attributes.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-    public class SettingAttribute : Attribute, ISetting
+    public abstract class AbstractSettingAttribute : Attribute
     {
-        /// Name of the setting
-        public string Name { get; set; }
+        protected object _defaultValue = null;
+        protected bool   _hasDefaultValue = false;
+
         /// Description of the setting
         public string Description { get; set; }
-        /// The Type of value which the setting accepts
-        public Type ParameterType { get; set; }
+        /// Whether the parameter has a DefaultValue (since the default may be null)
+        public bool   HasDefaultValue { get { return _hasDefaultValue; } }
+        /// Default value for parameter
+        public object DefaultValue {
+            get {
+                return _defaultValue;
+            }
+            set {
+                _defaultValue = value;
+                _hasDefaultValue = true;
+            }
+        }
+        /// Whether the setting has a DefaultValue; settings default to true
+        public bool   IsSensitive { get; set; }
         /// The constant prefix of all values in the enum which can be omitted
         public string EnumPrefix { get; set; }
-        /// Whether the setting is sensitive, and should be masked
-        public bool IsSensitive { get; set; }
-        /// Whether the setting has a DefaultValue; settings default to true
-        public bool HasDefaultValue { get; set; }
-        /// Default value for setting (may be null, even if this is not the
-        /// default)
-        // TODO: Do we need another field to indicate if default is available?
-        public object DefaultValue { get; set; }
-        /// Version in which the setting was introduced
-        public string Since { get; set; }
-        /// Version in which the setting was deprecated
-        public string Deprecated { get; set; }
 
-        public SettingAttribute()
-        {
-            ParameterType = typeof(bool);
-            HasDefaultValue = true;
-        }
-
-        public SettingAttribute(string name)
-        {
-            Name = name;
-            ParameterType = typeof(bool);
-            HasDefaultValue = true;
-        }
     }
 
 
+
     /// <summary>
-    /// Define an attribute which will be used to specify additional parameter
+    /// Define an attribute which will be used to specify command parameter
     /// information, such as default values, sensitive flags, enum prefixes,
     /// etc.
     /// </summary>
@@ -108,27 +98,38 @@ namespace Command
     /// they can appear (only at the end of the list of parameters).
     /// </remarks>
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
-    public class ParameterAttribute : Attribute
+    public class ParameterAttribute : AbstractSettingAttribute
     {
-        private object _defaultValue = null;
-        private bool   _hasDefaultValue = false;
-
-        public string Description { get; set; }
-        public bool   HasDefaultValue { get { return _hasDefaultValue; } }
-        public object DefaultValue {
-            get {
-                return _defaultValue;
-            }
-            set {
-                _defaultValue = value;
-                _hasDefaultValue = true;
-            }
-        }
-        public bool   IsSensitive { get; set; }
-        public string EnumPrefix { get; set; }
-
         public ParameterAttribute(string desc) {
             Description = desc;
+        }
+    }
+
+
+
+    /// <summary>
+    /// Used to define the possible settings that can appear in an
+    /// ISettingsCollection subclass. These often change between versions, so
+    /// Since and Deprecated arguments are supported.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class SettingAttribute : AbstractSettingAttribute, ISetting
+    {
+        /// Name of the setting
+        public string Name { get; set; }
+        /// The Type of value which the setting accepts
+        public Type ParameterType { get; set; }
+        /// Version in which the setting was introduced
+        public string Since { get; set; }
+        /// Version in which the setting was deprecated
+        public string Deprecated { get; set; }
+
+
+        public SettingAttribute(string name)
+        {
+            Name = name;
+            ParameterType = typeof(bool);
+            _hasDefaultValue = true;
         }
     }
 
