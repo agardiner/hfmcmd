7a5b609e7e77b6b7b7e10b0196043fddc1156e4d
Keith Kikta <kkikta@gmail.com>
2013-12-12 14:41:27 -0600

    Added files for testing project in Visual Studio

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HFM Command")]
+[assembly: AssemblyDescription("A command-line interface to Oracle Hyperion Financial Management.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://github.com/agardiner/hfmcmd")]
+[assembly: AssemblyProduct("HFMCmd for 11.1.2.2 (unknown)")]
+[assembly: AssemblyCopyright("Adam Gardiner, 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.4.0.1")]
+[assembly: AssemblyFileVersion("0.4.0.1")]
